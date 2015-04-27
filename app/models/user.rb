12d25d0f1d926cc1bdea9c937b0b264715f46ddd
Alex Knoll & Jdeesy <pair+alexandergknoll.jdeesy@devbootcamp.com>
2015-04-27 09:05:19 -0500

    add user model and migration

---
+++ app/models/user.rb
@@ -0,0 +1,18 @@
+class User < ActiveRecord::Base
+  validates :name, :email, :password, presence: true
+
+  def password
+    @password ||= BCrypt::Password.new(self.password_digest)
+  end
+
+  def password=(user_input)
+    @password = BCrypt::Password.create(user_input)
+    self.password_digest = @password
+  end
+
+  def self.authenticate(email, password)
+    @current_user = User.find_by_email(email)
+    return @current_user if @current_user && @current_user.password == password
+  end
+
+end
